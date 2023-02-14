a5d32616328da26d8355b14676e0cbcf92f12f19
SQLAdmin <abuyeahia24@gmail.com>
2023-02-15 03:40:11 +0600

    update case operation added

--- shopping-cart-main/js/case.js
+++ shopping-cart-main/js/case.js
@@ -1,12 +1,64 @@
-document.getElementById('btn-cas-plus').addEventListener('click', function(){
-    // console.log('case button click')
+
+
+
+function updateCaseNumber(isIncrease) {
 
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
-    console.log(previousCaseNumber);
+   
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
 
-    const newCaseNumber = previousCaseNumber + 1;
+    }
+    else if ( previousCaseNumber <= 0) {
+        alert("Negative Value is not allowed");
+        return newCaseNumber;
+      }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
+}
 
-})
+
+
+
+
+// // add event listener to the case plus button
+// document.getElementById('btn-cas-plus').addEventListener('click', function () {
+//     // console.log('case button click')
+//     //  get the value inside the case number field (input field)
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberString = caseNumberField.value;
+//     // convert number to an integer
+//     const previousCaseNumber = parseInt(caseNumberString);
+//     // console.log(previousCaseNumber);
+//     // calculate the new case number
+//     const newCaseNumber = previousCaseNumber + 1;
+//     // set the value to the case number field
+//     caseNumberField.value = newCaseNumber;
+
+// })
+
+document.getElementById('btn-cas-plus').addEventListener('click', function () {
+updateCaseNumber(true);
+});
+
+
+// document.getElementById('btn-case-minus').addEventListener('click', function () {
+//     // console.log('case minus added')
+
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberString = caseNumberField.value;
+//     const previousCaseNumber = parseInt(caseNumberString);
+//     // console.log(previousCaseNumber);
+
+//     const newCaseNumber = previousCaseNumber - 1;
+//     caseNumberField.value = newCaseNumber;
+// })
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+updateCaseNumber(false);
+});
